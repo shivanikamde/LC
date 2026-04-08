9a00d08e35360421211aacfeb45ed929dfb29a11
shivanikamde <84928027+shivanikamde@users.noreply.github.com>
2026-04-08 21:54:44 +0530

    Time: 6 ms (96.68%), Space: 45.2 MB (21.92%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        int n=nums.length;
+        String elements[]=new String[n];
+        for(int i=0;i<n;i++){
+            elements[i]=Integer.toString(nums[i]);
+        }
+
+        Arrays.sort(elements,new Comparator<String>(){
+            public int compare(String a, String b){
+                String first = a + b;
+                String second = b + a;
+                return second.compareTo(first);
+            }
+        });
+        if(elements[0].equals("0")){
+            return "0";
+        }
+        String res="";
+        for(String val: elements){
+            res+=val;
+        }
+        return res;
+    }
+}
