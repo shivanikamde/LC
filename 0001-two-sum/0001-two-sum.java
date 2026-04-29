80cfbd4ed3ae3e8e928b074e4b533dc96943952e
shivanikamde <84928027+shivanikamde@users.noreply.github.com>
2026-04-29 14:40:00 +0530

    Time: 2 ms (99.29%), Space: 46.5 MB (95.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        int find = 0;
-        for ( int i = 0 ; i < nums.length; i++ ){
-            find = target - nums[i];
+        //better approach : using hashmap
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        int find=0;
+
+        for(int i=0;i<nums.length;i++){
+            find=target-nums[i];
+
             if(hm.containsKey(find)){
-                return new int[]{hm.get(find), i};
-            } else {
-                hm.put(nums[i], i);
+                return new int[]{hm.get(find),i};
+            }
+            else{
+                hm.put(nums[i],i);
             }
         }
-
         return new int[]{-1};
+
+        // brute approach : using all possible combo
+        // int length=nums.length;
+        // for(int i=0;i<length;i++){
+        //     for(int j=i+1;j<length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[]{-1,-1};
+
+        //optimal approach using greedy
     }
 }
